6bcfd131bea685746afd029093df82ab0109d321
thanhvinh-htnbt <thanhvinh.htn2020@gmail.com>
2023-12-03 11:39:57 +0700

    Run

---
+++ app/src/main/java/com/example/projectalbum/Adapter/ImagePagerAdapter.java
@@ -0,0 +1,80 @@
+package com.example.projectalbum.Adapter;
+
+import static com.example.projectalbum.Database.DB.getListPhoto;
+import com.example.projectalbum.Database.DB;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.bumptech.glide.Glide;
+import com.example.projectalbum.Model.Photo;
+import com.example.projectalbum.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImagePagerAdapter extends PagerAdapter {
+    private Context context;
+    private List<Photo> photoList;
+
+    int index=0;
+    String imagePathx;
+
+    public ImagePagerAdapter(Context context, List photoList, String imagePath) {
+        this.context = context;
+        this.photoList = photoList;
+        this.imagePathx=imagePath;
+    }
+
+    public int getIndex(String imagePath) {
+        for(int i=0;i<photoList.size();i++)
+        {
+            if(photoList.get(i).getfilePath()==imagePath)
+            {
+                index=i;
+            }
+        }
+
+        return index;
+    }
+
+    @Override
+    public int getCount() {
+        return photoList.size();
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_image, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
+
+        //position=getIndex(imagePathx);
+
+        String imagePath = photoList.get(position).getfilePath();
+
+        Glide.with(context).load(imagePath).into(imageView);
+
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((View) object);
+    }
+}
